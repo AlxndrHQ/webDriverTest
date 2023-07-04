7c62db4b1124515ec1270cf6c1699e355e845b87
alxndrhq@gmail.com <alxndrhq@gmail.com>
2023-07-04 18:38:06 -0400

    "added mac lines";

--- webDriverTest/src/chromeBrowser/launchApplication.java
+++ webDriverTest/src/chromeBrowser/launchApplication.java
@@ -8,11 +8,15 @@ public class launchApplication {
 
 	// Main method (Driver function)
 	public static void main(String[] args) {
-		// Sets driver/ tells what driver needs to be loaded 
+		// Sets driver/ tells what driver needs to be loaded (For PC)
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\steven.williams.jr\\Downloads\\chromedriver_win32/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();		
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\steven.williams.jr\\Downloads\\chromedriver_win32/chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();		
 		
+		// Sets driver/ tells what driver needs to be loaded (For Mac)
+		System.setProperty("webdriver.chrome.driver", "Mackintosh HD\\Users\\stevenjr.\\Downloads\\chromedriver_mac64/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();		
+
 		//System.setProperty("webdriver.gecko.driver","C:\\Users\\steven.williams.jr\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
 		//WebDriver.driver = new FirefoxDriver();
 		
